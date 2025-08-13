ff0cd84218d0689d01c337342ab753ac493d573b
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-08-13 12:12:37 -0300

    add: implmentt

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,5 +2,5 @@
 
 Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Multiplication.Mul(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
